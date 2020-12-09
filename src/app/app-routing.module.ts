621ddf25ca44647d20b7fef9228a5236c9d52fab
Mjanaina <mariajanainadasilva567@gmail.com>
2020-12-09 19:04:08 -0300

    feed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,30 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'cadastro',
-    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'detalhes-evento',
-    loadChildren: () => import('./detalhes-evento/detalhes-evento.module').then( m => m.DetalhesEventoPageModule)
-  },
-  {
-    path: 'configuracoes',
-    loadChildren: () => import('./configuracoes/configuracoes.module').then( m => m.ConfiguracoesPageModule)
-  },
-  {
-    path: 'lista-usuarios',
-    loadChildren: () => import('./lista-usuarios/lista-usuarios.module').then( m => m.ListaUsuariosPageModule)
-  }
+  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)},
+  { path: 'cadastro', loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)},
+  { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)},
+  { path: 'detalhes-evento/:id', loadChildren: () => import('./detalhes-evento/detalhes-evento.module').then( m => m.DetalhesEventoPageModule)},
+  { path: 'configuracoes', loadChildren: () => import('./configuracoes/configuracoes.module').then( m => m.ConfiguracoesPageModule)},
+  { path: 'lista-usuarios', loadChildren: () => import('./lista-usuarios/lista-usuarios.module').then( m => m.ListaUsuariosPageModule)}
 ];
 @NgModule({
   imports: [
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Evento } from 'src/app/Interfaces/evento';
+import { EventoService } from 'src/app/Services/evento.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+  private eventos = new Array<Evento>();
+  private eventosSubs: Subscription;
+  private eventoId: string = null;
 
-  constructor() {}
+  constructor(
+    private eventoServ: EventoService
+  ) {
+    this.eventosSubs = this.eventoServ.getEventos().subscribe(data => {
+      this.eventos = data;
+    })
+  }
 
+  ngOnInit() { }
+
+  ngOnDestroy() {
+    this.eventosSubs.unsubscribe();
+  }
 }
