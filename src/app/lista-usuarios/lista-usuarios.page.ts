8decafe8d3bae3ce39008bfea60adc47eb1b11af
Mjanaina <mariajanainadasilva567@gmail.com>
2020-12-10 07:35:20 -0300

    barra de pesquisa/aba seguir

--- src/app/lista-usuarios/lista-usuarios.page.ts
+++ src/app/lista-usuarios/lista-usuarios.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/Interfaces/usuario';
 import { UsuarioService } from 'src/app/Services/usuario.service';
 import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators'
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 @Component({
   selector: 'app-lista-usuarios',
@@ -11,9 +14,11 @@ import { Subscription } from 'rxjs';
 export class ListaUsuariosPage implements OnInit {
   private usuarios = new Array<Usuario>();
   private usuariosSubs: Subscription
-  
+  public pesquisa: string = '';
+
   constructor(
-    private usuarioServ: UsuarioService
+    private usuarioServ: UsuarioService,
+    private firestore: AngularFirestore
   ) { 
     this.usuariosSubs = this.usuarioServ.getUsuarios().subscribe(data => {
       this.usuarios = data;
@@ -21,9 +26,30 @@ export class ListaUsuariosPage implements OnInit {
   }
 
   async ngOnInit() {
+    this.usuarios = await this.inicializaItens();
+  }
+
+ async inicializaItens(): Promise <any> {
+    const listaUsuarios = await this.firestore.collection('Usuários').valueChanges().pipe(first()).toPromise();
+    return listaUsuarios;
   }
 
   ngOnDestroy() {
     this.usuariosSubs.unsubscribe();
+  }
+
+  async filterList(evt) {
+    this.usuarios = await this.inicializaItens();
+    this.pesquisa = evt.srcElement.value;
+  
+    if (!this.pesquisa) {
+      return;
+    }
+
+    this.usuarios = this.usuarios.filter(usuario => {
+      if (usuario.username && this.pesquisa) {
+        return (usuario.username.toLowerCase().indexOf(this.pesquisa.toLowerCase()) > -1);
+      }
+    })
   }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/Interfaces/usuario';
+import { UsuarioService } from 'src/app/Services/usuario.service';
+import { Subscription } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +11,47 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  private usuarios = new Array <Usuario>();
+  private usuariosSubs: Subscription;
+  private usuarioId: string = null
+  private usuario = {};
 
-  constructor() {}
+  constructor(
+    private fireauth: AngularFireAuth,
+    private usuarioServ: UsuarioService,
+    private router: Router
+  ) {
+    this.usuariosSubs = this.usuarioServ.getUsuarios().subscribe(data => {
+      this.usuarios = data;
+    })
+
+    this.filterUsuarios();
+  }
+
+  async filterUsuarios() {
+    const userUid = (await this.fireauth.currentUser).uid
+  
+    for (let i = 0; i < this.usuarios.length; i++) {
+        if (this.usuarios[i].uid === userUid) {
+          this.usuarioId = this.usuarios[i].uid
+          try {
+           await this.usuarioServ.getUsuario(this.usuarioId);
+          } catch(error) {
+            console.log(error);
+            
+          }
+        }
+    }
+  }
+
+  async logout() {
+    try {
+      await this.fireauth.signOut();
+      this.router.navigate(['/login'])
+    } catch(error) {
+      console.log(error);
+      
+    }
+  }
 
 }
